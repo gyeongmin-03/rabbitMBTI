72824cbe742da458cbaf77f8a237f0bced1a2db8
gyeongmin-03 <101932591+gyeongmin-03@users.noreply.github.com>
2022-12-21 12:23:32 +0900

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,86 @@
+const qnaList = [
+    {
+        q: '1. ',
+        a: [
+            { answer: 'a. ', type: ['E']},
+            { answer: 'b. ', type: ['I']},
+        ]
+    },
+    {
+        q: '2. ',
+        a: [
+            { answer: 'a.', type: ['E']},
+            { answer: 'b. ', type: ['I']},
+        ]
+    },
+    {
+        q: '3. ',
+        a: [
+            { answer: 'a. ', type: ['I']},
+            { answer: 'b.', type: ['E']},
+        ]
+    },
+    {
+        q: '4.',
+        a: [
+            { answer: 'a. ', type: ['N']},
+            { answer: 'b. ', type: ['S']},
+        ]
+    },
+    {
+        q: '5. ',
+        a: [
+            { answer: 'a. ', type: ['N']},
+            { answer: 'b. ', type: ['S']},
+        ]
+    },
+    {
+        q: '6. ',
+        a: [
+            { answer: 'a. ', type: ['N']},
+            { answer: 'b. ', type: ['S']},
+        ]
+    },
+    {
+        q: '7. ',
+        a: [
+            { answer: 'a. ', type: ['F']},
+            { answer: 'b. ', type: ['T']},
+        ]
+    },
+    {
+        q: '8. ',
+        a: [
+            { answer: 'a. ', type: ['F']},
+            { answer: 'b. ', type: ['T']},
+        ]
+    },
+    {
+        q: '9.',
+        a: [
+            { answer: 'a. ', type: ['F']},
+            { answer: 'b. ', type: ['T']},
+        ]
+    },
+    {
+        q: '10. ',
+        a: [
+            { answer: 'a. ', type: ['P']},
+            { answer: 'b. ', type: ['J']},
+        ]
+    },
+    {
+        q: '11.',
+        a: [
+            { answer: 'a. ', type: ['J']},
+            { answer: 'b. ', type: ['P']},
+        ]
+    },
+    {
+        q: '12. ',
+        a: [
+            { answer: 'a. ', type: ['J']},
+            { answer: 'b. ', type: ['P']},
+        ]
+    },
+]
